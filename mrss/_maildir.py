b7babd56bce80df7ed3aa9b80561186f73f0e2d7
zsugabubus <zsugabubus@users.noreply.github.com>
2023-04-14 23:01:42 +0200

    feat: Add create parameter to Maildir

--- mrss/_maildir.py
+++ mrss/_maildir.py
@@ -5,16 +5,17 @@ import os
 
 
 class Maildir(Mailbox):
-    def __init__(self, *, path: Path, **kwargs):
-        # Maildir([create=True]) creates subdirs only if path does not exist.
-        for subdir in ["cur", "new", "tmp"]:
-            try:
-                os.mkdir(os.path.join(path, subdir))
-            except FileExistsError:
-                pass
-            except FileNotFoundError:
-                pass
+    def __init__(self, *, path: Path, create: bool = True, **kwargs):
+        if create:
+            # Maildir([create=True]) creates subdirs only if path does not exist.
+            for subdir in ["cur", "new", "tmp"]:
+                try:
+                    os.mkdir(os.path.join(path, subdir), 0o700)
+                except FileExistsError:
+                    pass
+                except FileNotFoundError:
+                    pass
         super().__init__(
-            mailbox=mailbox.Maildir(path),
+            mailbox=mailbox.Maildir(path, create=create),
             **kwargs,
         )
